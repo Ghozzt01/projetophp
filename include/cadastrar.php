2ef8283c4c146c1863db3d66ed36ef44d8220822
Guilherme Lopes <142898138+Ghozzt01@users.noreply.github.com>
2025-10-21 22:17:03 -0300

    asdkakdjlaskdalsd

---
+++ include/cadastrar.php
@@ -0,0 +1,19 @@
+<?php 
+	
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	
+	$primeiroNome = $_POST['primeiroNome'];
+	$segundoNome = $_POST['segundoNome'];
+	$escolhaPets = $_POST['escolhaPets'];
+
+
+	echo($primeiroNome . " - " . $segundoNome ." - " . $escolhaPets);
+
+	echo '<br><br><a href="../index.php">Voltar</a>';
+
+	//header("location ../index.php")
+}else
+{
+	echo "é post";
+	}
+ ?>
